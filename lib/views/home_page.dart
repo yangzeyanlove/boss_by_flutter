440117b1d320499454fd662e42ae515f63a2f70d
yangzy01 <yangzy01@mingyuanyun.com>
2024-09-14 17:20:23 +0800

    feat: 切换底部导航时，保持页面状态

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -33,7 +33,11 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pages[_currentIndex],
+      // body: _pages[_currentIndex],
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
